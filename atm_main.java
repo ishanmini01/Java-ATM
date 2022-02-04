ed7274a14f94586de7021264d35e4eb44ea4c0e9
ishanmini01 <80204020+ishanmini01@users.noreply.github.com>
2022-02-04 22:00:36 +0530

    Add files via upload

---
+++ atm_main.java
@@ -0,0 +1,10 @@
+package ATM;
+import java.io.*;
+
+public class atm_main extends OptionMenu {
+
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+		optionMenu.getLogin();
+	}
+}
